e57217bc1b8a38d2e5b7f97470772968d4ee87cb
jasnmar <jasnmar@gmail.com>
2024-04-15 12:56:49 -0400

    Progress, again

--- index.js
+++ index.js
@@ -1,99 +1,245 @@
 import { key } from "./apikey.js"
 
+//Provides context to some functions as to
+//whether or not we are on the search page
+//or the watch list
+const page = document.body.dataset.pageid
+
+//Search Button Element
 const searchBtn = document.getElementById("search-btn")
+
 let movieArray = []
+
+//If we're on the watch list, do default things
+if(page==="watchlist") {
+    setupWatchlist()
+}
 
 if(searchBtn) {
     searchBtn.addEventListener("click", getMovieList)
 }
 
+async function setupWatchlist() {
+    const watchList = localStorage.getItem("movieList")
+    //If there are items in the local watchlist, display them
+    if(watchList) {
+        //The local watch list is only as deep as the search result
+        //so we need to get the details before displaying them.
+        const movieDetailArray = await getMovieDetails(JSON.parse(watchList))
+        renderMovieCards(movieDetailArray, page)
+    }
+}
 
+//Called by the Search button on the Search Page
 async function getMovieList(e) {
     e.preventDefault()
+    //Get the search term
     let searchTerm = document.getElementById("search").value
+    
     //TODO: Remove this, here for debugging only
     if(searchTerm == "") {
         searchTerm = "Blade Runner"
     }
+    //Replace spaces for the query param
     searchTerm = searchTerm.replace(" ","+")
 
+    //Call the API and wait for data
     const res = await fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`)
     const data = await res.json()
-    console.log(data)
+    //Set the movie array to the resulst of the search
     movieArray = data.Search
-    console.log(movieArray)
+    //Get the details of each movie
+    //The basic search doesn't include the plot, which was required
+    //by the design
     const moviewDetailArray = await getMovieDetails(movieArray)
-    console.log(moviewDetailArray)
-    let divText = ""
-    //TODO: Deal with a movie poster that doesn't exist
-    moviewDetailArray.forEach(movie => {
-        divText += `
-            <div data-imdbid="${movie.imdbID}" class="movie-card">
-                <img class="poster" src="${movie.Poster}">
-                <div class="movie-details">
-                    <div class="movie-lead">
-                        <p class="movie-title">${movie.Title}</p>
-                        <img class="star" src="./images/star.png">
-                        <p class="rating">${movie.imdbRating}
-                    </div>
-                    <div class="metadata">
-                        <p class="runtime">${movie.Runtime}</p>
-                        <p class="genre">${movie.Genre}</p>
-                        <button class="card-action-btn" data-imdbid="${movie.imdbID}">
-                            <img class="card-action-img" src="./images/PlusIconBlack.png">Watchlist</button>
-                        </button>
-                    </div>
-                    <div class="plot"><p class="plot">${movie.Plot}
-                    </div>
-                </div>
-            </div>
-        `
-    });
-    const mainContent = document.getElementById("main")
-    mainContent.innerHTML = divText
-    const cardDivList = document.querySelectorAll(".movie-card")
-    for(let cardDiv of cardDivList){
-        cardDiv.addEventListener("click", addMovieListener)
-    }
+    //Display the cards
+    renderMovieCards(moviewDetailArray, page)
 }
 
+//Gets movie details from the API for all of the movies
+//This operates on both the search results and the
+//items stored in local storage. 
 async function getMovieDetails(movies) {
+    //Empty array to store details in
     const mDetails = []
+    //Lookup each movie based on it's IMDB number
     for (let movie of movies) {
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${key}&i=${movie.imdbID}`)
-        const data = await res.json()
-        mDetails.push(data)
+        if(movie) {
+            const res = await fetch(`http://www.omdbapi.com/?apikey=${key}&i=${movie.imdbID}`)
+            const data = await res.json()
+            mDetails.push(data)
+        }
     }
     return mDetails
 }
 
-function addMovieListener(e) {
-    console.log(e.target.tagName)
-    if(e.target.tagName=='BUTTON'){
-        addMovieToLocalStorage(e.target.dataset.imdbid)
+function renderMovieCards(detailMovieArray, context) {
+    console.log('renderMovieCards', detailMovieArray)
+    if(detailMovieArray.length===0) {
+        renderEmptyState(context)
     } else {
-        console.log("Nope")
+
+        //Depending on whether we are rendering the search page
+        //or the watch list, the button HTML is different. This
+        //accounts for that
+        let buttonHTML = ""
+        if(context==="searchpage") {
+            buttonHTML = `
+            <img class="card-action-img" src="./images/PlusIconBlack.png">Watchlist</button>
+            `
+        } else {
+            buttonHTML = `
+            <img class="card-action-img" src="./images/MinusIconBlack.png">Remove</button>
+        `
+    }
+    //Variable that holds the entire text of the cards portion
+    let cardsHTML = ""
+    detailMovieArray.forEach(movie => {
+        console.log(movie)
+        //There are some cases where the poster is blank.
+        //When that happens the API returns N/A
+        //Check for that and display a placeholder image in 
+        //cases where there isn't an image (otherwise we get an
+        //error in the console)
+        let posterHTML = ""
+        if(movie.Poster!="N/A") {
+            posterHTML = `
+            <img class="poster" src="${movie.Poster}">
+            `
+        } else {
+            posterHTML = `
+            <img class="poster" src="https://placehold.co/100x150">
+            `
+        }
+        //Create the HTML for a specific card
+        cardsHTML += `
+        <div data-imdbid="${movie.imdbID}" class="movie-card">
+        ${posterHTML}
+        <div class="movie-details">
+        <div class="movie-lead">
+        <p class="movie-title">${movie.Title}</p>
+        <img class="star" src="./images/star.png">
+        <p class="rating">${movie.imdbRating}
+        </div>
+        <div class="metadata">
+        <p class="runtime">${movie.Runtime}</p>
+        <p class="genre">${movie.Genre}</p>
+        <button class="card-action-btn" data-imdbid="${movie.imdbID}">
+        ${buttonHTML}
+        </button>
+        </div>
+        <div class="plot"><p class="plot">${movie.Plot}
+        </div>
+        </div>
+        </div>
+        `
+    });
+    //On both the search and list pages we replace the contents
+    //the main div with the HTML generated here.
+    //TODO: deal with empty states (no search results and no
+    //Items in the watchlist)
+    const mainContent = document.getElementById("main")
+    mainContent.innerHTML = cardsHTML
+    //Add event listeners to the cards
+    const cardDivList = document.querySelectorAll(".movie-card")
+    for(let cardDiv of cardDivList){
+        cardDiv.addEventListener("click", movieListener)
+    }
     }
 }
 
-//TODO: Check to see if it is in local storage first
-function addMovieToLocalStorage(imdbId) {
-    let cLocalStorage = localStorage.getItem("movieList")
-    cLocalStorage = JSON.parse(cLocalStorage)
-    const newItem = {
-        "imdbID": imdbId
+function renderEmptyState(context) {
+    let emptyHTML = ""
+    if(context === "watchlist") {
+        emptyHTML = `
+            <div class="empty-state">
+                <p>Your watchlist is looking a little empty...</p>
+                <button class="add-movies-btn"><img class="card-action-img" src="./images/PlusIconBlack.png">Let's Add some movies</button>
+            </div>
+        `
+    } else {
+
     }
-    console.log(newItem)
+    const mainContent = document.getElementById("main")
+    mainContent.innerHTML = emptyHTML
+}
+
+
+//Event listener for Add and Remove buttons
+function movieListener(e) {
+    //Remove on the watch list
+    if(page==="watchlist") {
+        if(e.target.tagName=='BUTTON'){
+            removeMovieFromLocalStorage(e.target.dataset.imdbid)
+        }
+    //Add on the search page
+    } else {
+        if(e.target.tagName=='BUTTON'){
+            addMovieToLocalStorage(e.target.dataset.imdbid)
+        }
+    }
+}
+
+async function removeMovieFromLocalStorage(imdbId) {
+    //Get the movies from local storage
+    const cLocalStorage = JSON.parse(localStorage.getItem("movieList"))
+    //Get the index of the clicked movie
+    let itemIndex = -1
+    for (let ii=0; ii<cLocalStorage.length; ii++) {
+        if(cLocalStorage[ii]) {
+            if(cLocalStorage[ii].imdbID === imdbId) {
+                itemIndex = ii
+            }
+        }
+    }
+    //If we found the movie, remove it
+    if(itemIndex>=0) {
+        cLocalStorage.splice(itemIndex,1)
+    }
+    //Rewrite local storage
+    localStorage.setItem("movieList", JSON.stringify(cLocalStorage))
+    //Render the cards
+    const movieDetailArray = await getMovieDetails(cLocalStorage)
+    renderMovieCards(movieDetailArray, page)
+}
+
+
+function addMovieToLocalStorage(imdbId) {
+    //Get local storage
+    const cLocalStorage = JSON.parse(localStorage.getItem("movieList"))
+    const newItem = getMovieFromList(movieArray, imdbId)
     if(cLocalStorage) {
-        cLocalStorage.push(newItem)
-        localStorage.setItem("movieList", JSON.stringify(cLocalStorage))
+        const movieExists = cLocalStorage.some((movie) => movie.imdbID === imdbId)
+        if(!movieExists) {
+            cLocalStorage.push(newItem)
+            localStorage.setItem("movieList", JSON.stringify(cLocalStorage))
+        } else {
+            console.log("Movie exists")
+        }
     } else {
         localStorage.setItem("movieList", JSON.stringify([newItem]))
     }
-    console.log(cLocalStorage)
-
-
 }
+
+function getMovieFromList(array, imdbNumber) {
+
+    console.log("imdbNumber: "+ imdbNumber)
+    const foundItem = array.find((movie) => {
+        if(movie) {
+            if(movie.imdbID === imdbNumber) {
+                console.log("Found Movie: "+JSON.stringify(movie))
+                return movie
+            }
+        }
+    })
+    return foundItem
+}
+
+
+
+
+
+
 /*
 {
     "Search": [
